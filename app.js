7206d1f12d72879779069f0f29b44188d5a05353
Bahar Hosseini <bahar.hosseini@gmail.com>
2021-11-24 18:01:19 -0500

    first commit

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 //jshint esversion:6
 require('dotenv').config();
 const express = require("express");
-const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const path=require('path')
 const mongoose = require("mongoose");
 const session = require('express-session');
 const passport = require("passport");
@@ -14,9 +14,8 @@ const app = express();
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.set('views',path.join(__dirname,'views'))
+app.use(express.urlencoded({extended:true}))
 
 app.use(session({
   secret: "Our little secret.",
